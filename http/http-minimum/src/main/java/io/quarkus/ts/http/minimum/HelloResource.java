18973599f50a2b0f153e17e32354b4b4dd155b18
Kyrylo Shpak <kshpak@redhat.com>
2021-11-30 13:49:21 +0100

    Test new actions/set-java@v2 with cache

--- http/http-minimum/src/main/java/io/quarkus/ts/http/minimum/HelloResource.java
+++ http/http-minimum/src/main/java/io/quarkus/ts/http/minimum/HelloResource.java
@@ -2,6 +2,8 @@ package io.quarkus.ts.http.minimum;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -16,4 +18,17 @@ public class HelloResource {
     public Hello get(@QueryParam("name") @DefaultValue("World") String name) {
         return new Hello(String.format(TEMPLATE, name));
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hello post(Hello hello) {
+        return hello;
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hello put(Hello hello) {
+        return hello;
+    }
+
 }
